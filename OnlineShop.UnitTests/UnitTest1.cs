716c6f4f227e2fed27b5f17b0fdc3706b29d732b
Cezary Łuksza <c_luksza@WWSI-0609>
2018-12-10 14:50:56 +0100

    added paginaton

--- OnlineShop.UnitTests/UnitTest1.cs
+++ OnlineShop.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using OnlineShop.Domain.Abstract;
+using OnlineShop.Domain.Entities;
+using OnlineShop.WebUI.Controllers;
 
 namespace OnlineShop.UnitTests
 {
@@ -7,8 +13,32 @@ namespace OnlineShop.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1"},
+                new Product { ProductId = 2, Name = "P2"},
+                new Product { ProductId = 3, Name = "P3"},
+                new Product { ProductId = 4, Name = "P4"},
+                new Product { ProductId = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object)
+            {
+                PageSize = 3
+            };
+
+            //Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
--- OnlineShop.WebUI/Controllers/ProductController.cs
+++ OnlineShop.WebUI/Controllers/ProductController.cs
@@ -11,15 +11,16 @@ namespace OnlineShop.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-       
+        public int PageSize = 4;
+
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List() //metoda akcji, która generuje widok pełnej listy produktów
+        public ViewResult List(int page = 1) //metoda akcji, która generuje widok pełnej listy produktów
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p => p.ProductId).Skip((page - 1) * PageSize).Take(PageSize));
         }
     }
 }
