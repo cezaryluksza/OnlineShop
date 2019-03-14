d47bc17f5ac7c2abb822fe151ee03ae73111ebbf
Cezary Łuksza <cezary.luksza@gmail.com>
2019-03-14 21:45:27 +0100

    nav categories tests

--- OnlineShop.UnitTests/UnitTest1.cs
+++ OnlineShop.UnitTests/UnitTest1.cs
@@ -74,7 +74,7 @@ namespace OnlineShop.UnitTests
         {
             //Arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new Product[] 
+            mock.Setup(m => m.Products).Returns(new Product[]
             {
                 new Product { ProductId = 1, Name = "P1"},
                 new Product { ProductId = 2, Name = "P2"},
@@ -124,6 +124,86 @@ namespace OnlineShop.UnitTests
             Assert.AreEqual(result.Length, 2);
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            //Arrange
+            // - create te mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product {ProductId = 1, Name = "P1", Category = "Apples"},
+                    new Product {ProductId = 2, Name = "P2", Category = "Apples"},
+                    new Product {ProductId = 3, Name = "P3", Category = "Plums"},
+                    new Product {ProductId = 4, Name = "P4", Category = "Oranges"}
+                });
+            NavController target = new NavController(mock.Object);
+
+
+            //Act - get the set of category
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            //Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product {ProductId = 1, Name = "P1", Category = "Apples"},
+                    new Product {ProductId = 2, Name = "P2", Category = "Oranges"},
+                });
+
+            NavController target = new NavController(mock.Object);
+
+            string categoryToSelect = "Apples";
+
+            //Act
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            //Assert
+            Assert.AreEqual(categoryToSelect, result);
+        }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product {ProductId = 1, Name = "P1", Category = "Cat1"},
+                    new Product {ProductId = 2, Name = "P2", Category = "Cat2"},
+                    new Product {ProductId = 3, Name = "P3", Category = "Cat1"},
+                    new Product {ProductId = 4, Name = "P4", Category = "Cat2"},
+                    new Product {ProductId = 5, Name = "P5", Category = "Cat3"}
+                });
+
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            //Act
+            int res1 = ((ProductsListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductsListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductsListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductsListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+            //Assert
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
         }
     }
 }
