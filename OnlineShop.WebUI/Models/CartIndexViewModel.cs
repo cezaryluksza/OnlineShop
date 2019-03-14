48d52400afcb801178eeb05966d6b3adbc668e01
Cezary Łuksza <cezary.luksza@gmail.com>
2019-03-14 21:45:05 +0100

    nav categories

---
+++ OnlineShop.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
