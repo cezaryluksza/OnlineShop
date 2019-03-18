f69d1539edf31d62b60d5ca5da00a2102cdf1e5f
Cezary Łuksza <cezary.luksza@gmail.com>
2019-03-18 21:31:09 +0100

    Cart

---
+++ OnlineShop.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Proszę podać nazwisko.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać pierwszy wiersz adresu.")]
+        [Display(Name = "Wiersz 1")]
+        public string Line1 { get; set; }
+        [Display(Name = "Wiersz 2")]
+        public string Line2 { get; set; }
+        [Display(Name = "Wiersz 3")]
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę miasta.")]
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę województwa")]
+        [Display(Name = "Województwo")]
+        public string State { get; set; }
+
+        [Display(Name = "Kod pocztowy:")]
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę kraju")]
+        [Display(Name = "Kraj:")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
--- OnlineShop.WebUI/Controllers/CartController.cs
+++ OnlineShop.WebUI/Controllers/CartController.cs
@@ -12,10 +12,12 @@ namespace OnlineShop.WebUI.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -47,6 +49,35 @@ namespace OnlineShop.WebUI.Controllers
                 cart.RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Koszyk jest pusty!");
+            }
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
