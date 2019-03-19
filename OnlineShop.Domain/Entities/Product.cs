f9b8a91136d5682374e91183b9c2164ee32c9192
Cezary Łuksza <cezary.luksza@gmail.com>
2019-03-19 21:35:07 +0100

    Product fix

--- OnlineShop.Domain/Entities/Product.cs
+++ OnlineShop.Domain/Entities/Product.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace OnlineShop.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductId { get; set; }
+        [Display(Name="Nazwa")]
         public string Name { get; set; }
+        [DataType(DataType.MultilineText), Display(Name = "Opis")]
         public string Description { get; set; }
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
+        [Display(Name = "Kategoria")]
         public string Category { get; set; }
     }
 }
