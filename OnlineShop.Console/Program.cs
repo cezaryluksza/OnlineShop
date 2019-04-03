6b069b0df687f2857e632b72a5702e149bb07e80
Cezary Łuksza <cezary.luksza@gmail.com>
2019-04-03 22:20:47 +0200

    add OnlineShop Console

---
+++ OnlineShop.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using OnlineShop.Domain.Concrete;
+using OnlineShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (EFDbContext context = new EFDbContext())
+                {
+                    var product = new Product()
+                    {
+                        Name = "Testowy produkt",
+                        Description = "Testowy opis",
+                        Price = 50.52M,
+                        Category = "Testowa kategoria"
+                    };
+                    context.Products.Add(product);
+                    context.SaveChanges();
+                }
+                System.Console.WriteLine("Press any key to continue...");
+                System.Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
