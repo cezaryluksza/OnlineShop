854d8bc99c0d582db40508fd188c3bd6a12f08fb
Cezary Łuksza <cezary.luksza@gmail.com>
2019-04-06 17:01:36 +0200

    improve categories

---
+++ OnlineShop.WebUI/Scripts/JavaScript.js
@@ -0,0 +1,9 @@
+﻿function toggleMenu() {
+    var menuBox = document.getElementById('menu-box');
+    if (menuBox.style.display == "block") {
+        menuBox.style.display = "none";
+    }
+    else { 
+        menuBox.style.display = "block";
+    }
+}
