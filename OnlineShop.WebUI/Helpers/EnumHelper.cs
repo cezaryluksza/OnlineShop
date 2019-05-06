84e9513077bbd0e399cf5542336a847aa639869a
Cezary Łuksza <cezary.luksza@gmail.com>
2019-05-06 19:49:08 +0200

    add EnumHelper and SEOHelper

---
+++ OnlineShop.WebUI/Helpers/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.WebUI.Helpers
+{
+    static public class EnumHelper
+    {
+        static public string GetDescription(System.Enum type)
+        {
+            var attrs = type.GetType().GetMember(type.ToString()).FirstOrDefault().GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attrs.Length > 0)
+            {
+                return ((DescriptionAttribute)attrs[0]).Description.ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ OnlineShop.WebUI/Helpers/SEOHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.WebUI.Helpers
+{
+    public static class SEOHelper
+    {
+        public static string EscapeUrl(string url)
+        {
+            return url.Replace(' ', '-');
+        }
+    }
+}
