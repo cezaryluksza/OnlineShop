ef1b5448a09b5970673ea79e0b8b634b36bcf774
Cezary Łuksza <cezary.luksza@gmail.com>
2019-05-27 22:36:06 +0200

    change sorting order

--- OnlineShop.WebUI/Models/SortingType.cs
+++ OnlineShop.WebUI/Models/SortingType.cs
@@ -10,71 +10,15 @@ namespace OnlineShop.WebUI.Models
     {
         [Description("Brak Sortowania")]
         NoSorting = 0,
+        [Description("Popularność")]
+        MostPopular = 1,
+        [Description("Nazwa A-Z")]
+        NameAZ = 2,
+        [Description("Nazwa Z-A")]
+        NameZA = 3,
         [Description("Najwyższa cena")]
-        HighestPrice = 1,
+        HighestPrice = 4,
         [Description("Najniższa cena")]
-        LowestPrice = 2,
-        [Description("Najbardziej popularne")]
-        MostPopular = 3,
-        [Description("Najwyżej oceniane")]
-        HighestRating = 4,
-        [Description("Największa liczba komentarzy")]
-        LargestNumberOfComments = 5,
+        LowestPrice = 5,
     }
-
-
-    //static public class SortingHelper
-    //{
-    //    public static SortingType GetEnumEnglishName(string polishName)
-    //    {
-    //        SortingType englishName = 0;
-    //        switch (polishName)
-    //        {
-    //            case "NajwyzszaCena":
-    //                englishName = SortingType.HighestPrice;
-    //                break;
-    //            case "NajnizszaCena":
-    //                englishName = SortingType.LowestPrice;
-    //                break;
-    //            case "Popularnosc":
-    //                englishName = SortingType.MostPopular;
-    //                break;
-    //            case "Ocena":
-    //                englishName = SortingType.HighestRating;
-    //                break;
-    //            case "Komentarze":
-    //                englishName = SortingType.LargestNumberOfComments;
-    //                break;
-    //            default:
-    //                break;
-    //        }
-    //        return englishName;
-    //    }
-
-    //    public static string GetEnumPolishName(SortingType englishName)
-    //    {
-    //        string polishName = string.Empty;
-    //        switch (englishName)
-    //        {
-    //            case SortingType.HighestPrice:
-    //                polishName = "NajwyzszaCena";
-    //                break;
-    //            case SortingType.LowestPrice:
-    //                polishName = "NajnizszaCena";
-    //                break;
-    //            case SortingType.MostPopular:
-    //                polishName = "Popularnosc";
-    //                break;
-    //            case SortingType.HighestRating:
-    //                polishName = "Ocena";
-    //                break;
-    //            case SortingType.LargestNumberOfComments:
-    //                polishName = "Komentarze";
-    //                break;
-    //            default:
-    //                break;
-    //        }
-    //        return polishName;
-    //    }
-    //}
 }
